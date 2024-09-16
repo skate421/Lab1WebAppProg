f0fc359d8092af62976dc2fe24d4dd949a69adb6
Skate421 <SKATIE13@outlook.com>
2024-09-16 11:18:24 -0300

    commitOne

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import contactsRouter from './routes/contacts.js';
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api/contacts', contactsRouter);
+
+/*app.get('/', (req, res) => {
+  res.send('Hello Express!');
+});*/
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ routes/contacts.js
@@ -0,0 +1,33 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('Contacts route!');
+});
+
+// to-do: add post, put, and delete routers
+router.post('/create', (req, res) => {
+  res.send('Create a contact');
+});
+
+router.put('/update', (req, res) => {
+  res.send('Update a contact');
+});
+
+router.delete('/delete', (req, res) => {
+  res.send('Delete a contact');
+});
+
+// Get all contacts
+router.get('/all', (req, res) => {
+  res.send('All contacts');
+});
+
+// Get a contact by id
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  res.send('Contact by id ' + id);
+});
+
+export default router;
