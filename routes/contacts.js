e3d044ec24d58ef96ee554ce58bd6977892cedc8
Skate421 <SKATIE13@outlook.com>
2024-09-17 08:55:59 -0300

    commit

--- routes/contacts.js
+++ routes/contacts.js
@@ -6,19 +6,6 @@ router.get('/', (req, res) => {
   res.send('Contacts route!');
 });
 
-// to-do: add post, put, and delete routers
-router.post('/create', (req, res) => {
-  res.send('Create a contact');
-});
-
-router.put('/update', (req, res) => {
-  res.send('Update a contact');
-});
-
-router.delete('/delete', (req, res) => {
-  res.send('Delete a contact');
-});
-
 // Get all contacts
 router.get('/all', (req, res) => {
   res.send('All contacts');
@@ -28,6 +15,19 @@ router.get('/all', (req, res) => {
 router.get('/:id', (req, res) => {
   const id = req.params.id;
   res.send('Contact by id ' + id);
+});
+
+// to-do: add post, put, and delete routers
+router.post('/create', (req, res) => {
+  res.send('Create a contact');
+});
+
+router.put('/update/:id', (req, res) => {
+  res.send('Update a contact');
+});
+
+router.delete('/delete/:id', (req, res) => {
+  res.send('Delete a contact');
 });
 
 export default router;
